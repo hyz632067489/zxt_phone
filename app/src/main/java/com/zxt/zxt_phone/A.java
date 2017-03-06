6523d0c7eff65b7ee80c3c457d2057ab1e4154c6
heyangzhou <472290422@qq.com>
2017-03-06 16:01:27 +0800

    1.新建登录活动

--- app/src/main/java/com/zxt/zxt_phone/A.java
+++
@@ -1,8 +0,0 @@
-package com.zxt.zxt_phone;
-
-/**
- * Created by miliang on 2017/3/6/0006.
- */
-
-public class A {
-}
---
+++ app/src/main/java/com/zxt/zxt_phone/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.zxt.zxt_phone;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by miliang on 2017/3/6/0006.
+ */
+
+public class LoginActivity extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
